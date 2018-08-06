f5638626bc7481918146f157ef35ebcd732c3fac
pronif <fede.proni@gmail.com>
2018-08-06 14:04:05 +0200

    add argument parsing

--- Colmap_openMVS/run_openMVS.py
+++ Colmap_openMVS/run_openMVS.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 import os
+import argparse
 
 import config
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--workspace_dir")
+parser.add_argument("--dry_run", dest="dry_run", action="store_true")
+parser.set_defaults(workspace_dir=config.workspace_dir)
+parser.set_defaults(dry_run=False)
+args = parser.parse_args()
 
 #input
 image_dir = config.image_dir
@@ -15,10 +23,8 @@ dense_mvs_file = config.workspace_dir + "/model_dense.mvs"
 unrefined_mesh_file = config.workspace_dir + "/model_dense_mesh.mvs"
 refined_mesh_file = config.workspace_dir + "/model_dense_mesh_refined.mvs"
 
-dry_run = True
 
-
-def execute_command(command, dry_run = True):
+def execute_command(command, dry_run=True):
     print()
     print(command)
     print()
@@ -28,28 +34,28 @@ def execute_command(command, dry_run = True):
 
 
 cmd = "colmap feature_extractor --database_path {} --image_path {} --SiftExtraction.use_gpu 0".format(colmap_database_file, image_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = "colmap exhaustive_matcher --database_path {} --SIFTMatching.use_gpu 0".format(colmap_database_file)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = "colmap mapper --database_path {} --image_path {} --export_path {}".format(colmap_database_file, image_dir, colmap_sparse_recon_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = "colmap model_converter {} --output_path {} --output_type NVM".format(colmap_sparse_recon_dir + "/0", sparse_nvm_file)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = config.openMVS_InterfaceSFM_bin + " -i {} -w {}".format(sparse_nvm_file, image_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = config.openMVS_Densify_bin + " {} -w {}".format(sparse_mvs_file, image_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = config.openMVS_Mesh_bin + " {} -w {}".format(dense_mvs_file, image_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = config.openMVS_Refine_bin + " {} -w {} --resolution-level 1".format(unrefined_mesh_file, image_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
 
 cmd = config.openMVS_Texture_bin + " {} -w {} --export-type obj".format(refined_mesh_file, image_dir)
-execute_command(cmd, dry_run)
+execute_command(cmd, args.dry_run)
