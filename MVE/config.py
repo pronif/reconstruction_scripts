cb9310212db1d9c8d24cda032b551e8c7d3af9f6
pronif <fede.proni@gmail.com>
2018-08-10 13:36:59 +0200

    fix typo

--- MVE/config.py
+++ MVE/config.py
@@ -1,9 +1,9 @@
 import os
 
 # Set the input folder (where the images are stored)
-input_folder = "/home/v4rl/data/Styrac"
+input_folder = "/home/v4rl/reconstruction/lawnmower_fixed_height/images"
 # Set the output folder (where the mve workspace will be)
-output_folder = "/home/v4rl/reconstruction/Styrac"
+output_folder = "/home/v4rl/reconstruction/fixed_height_mve"
 # Set the folder with the mve binaries
 mve_bin = "/home/v4rl/repo/mve/apps/"
 
@@ -11,7 +11,7 @@ mve_makescene_bin = mve_bin + "makescene/makescene"
 mve_sfmrecon_bin = mve_bin + "sfmrecon/sfmrecon"
 mve_dmrecon_bin = mve_bin + "dmrecon/dmrecon"
 mve_scene2pset_bin = mve_bin + "scene2pset/scene2pset"
-mve_fssrecon_bin = mve_bin + mve_bin + "fssrecon/fssrecon"
+mve_fssrecon_bin = mve_bin + "fssrecon/fssrecon"
 mve_meshclean_bin = mve_bin + "meshclean/meshclean"
 
 assert os.path.exists(mve_makescene_bin)
